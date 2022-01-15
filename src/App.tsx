4a0c68bed459a43a0de2eae348bc685a8782a5fb
Sami Honkasalo <honkasalo.sami@gmail.com>
2022-01-15 13:50:24 +0200

    Test pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Samin portfolio [Work In Progress]</p>
+        <p>Samin portfolio (Work In Progress)</p>
       </header>
     </div>
   );
