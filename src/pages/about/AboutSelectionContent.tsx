1ea26d80a3c02ee682ec3b82287911093e178cc2
Sami Honkasalo <honkasalo.sami@gmail.com>
2022-01-19 20:46:49 +0200

    Added some experience and education content

--- src/pages/about/AboutSelectionContent.tsx
+++ src/pages/about/AboutSelectionContent.tsx
@@ -1,4 +1,5 @@
 import BusinessIcon from "@mui/icons-material/Business";
+import SchoolIcon from "@mui/icons-material/School";
 import {
   Card,
   List,
@@ -42,10 +43,37 @@ const Experiences: ExperienceItemProps[] = [
     from: { month: 1, year: 2021 },
   },
   {
-    titleTranslationKey: "mock",
-    company: "Lorem Ipsum",
-    from: { month: 1, year: 2018 },
-    to: { month: 1, year: 2021 },
+    titleTranslationKey: "jamk",
+    company: "JAMK",
+    from: { month: 8, year: 2019 },
+  },
+  {
+    titleTranslationKey: "devecto",
+    company: "Devecto Oy",
+    from: { month: 12, year: 2019 },
+    to: { month: 12, year: 2020 },
+  },
+  {
+    titleTranslationKey: "previous",
+    company: "",
+    from: { month: 9, year: 2017 },
+    to: { month: 12, year: 2019 },
+    disabled: true,
+  },
+];
+
+const Educations: EducationItemProps[] = [
+  {
+    titleTranslationKey: "it",
+    school: "JAMK",
+    from: { month: 9, year: 2019 },
+    to: { month: 9, year: 2020 },
+  },
+  {
+    titleTranslationKey: "automation",
+    school: "JAMK",
+    from: { month: 8, year: 2014 },
+    to: { month: 1, year: 2018 },
   },
 ];
 
@@ -80,7 +108,7 @@ export const AboutSelectionContent = ({
             section={
               <List>
                 {Experiences.map((e) => (
-                  <ExperienceListItem key={e.company} {...e} />
+                  <ExperienceListItem key={e.titleTranslationKey} {...e} />
                 ))}
               </List>
             }
@@ -91,7 +119,13 @@ export const AboutSelectionContent = ({
           <EducationSectionWrapper
             prevKey={prevSectionKey}
             curKey={activeSectionKey}
-            section={<AboutMeText />}
+            section={
+              <List>
+                {Educations.map((e) => (
+                  <EducationListItem key={e.titleTranslationKey} {...e} />
+                ))}
+              </List>
+            }
           />
         );
 
@@ -143,63 +177,117 @@ type AboutTimespan = {
   from: MonthYear;
   to?: MonthYear;
 };
+
+type AboutListItemProps = {
+  icon: JSX.Element;
+  primary: string;
+  secondary: string;
+  ternary: JSX.Element;
+  disabled?: boolean;
+};
+function AboutListItem({
+  icon,
+  primary,
+  secondary,
+  ternary,
+  disabled,
+}: AboutListItemProps): JSX.Element {
+  const isMobile = useIsMobile();
+  return React.useMemo(
+    () => (
+      <ListItem disabled={disabled}>
+        <ListItemIcon sx={{ color: (theme) => theme.palette.common.black }}>
+          {icon}
+        </ListItemIcon>
+        <ListItemText
+          disableTypography
+          primaryTypographyProps={{
+            color: (theme) => theme.palette.common.black,
+            fontWeight: (theme) => theme.typography.fontWeightBold,
+            fontSize: (theme) => theme.typography.pxToRem(isMobile ? 14 : 16),
+          }}
+          secondaryTypographyProps={{
+            color: (theme) => theme.palette.common.black,
+          }}
+          primary={
+            <Typography
+              sx={{
+                color: (theme) => theme.palette.common.black,
+                fontWeight: (theme) => theme.typography.fontWeightBold,
+                fontSize: (theme) =>
+                  theme.typography.pxToRem(isMobile ? 14 : 16),
+              }}
+            >
+              {primary}
+            </Typography>
+          }
+          secondary={
+            <>
+              <Typography
+                component={isMobile ? "p" : "span"}
+                sx={{
+                  mr: 2,
+                  color: (theme) => theme.palette.grey[600],
+                }}
+              >
+                {secondary}
+              </Typography>
+              {ternary}
+            </>
+          }
+        />
+      </ListItem>
+    ),
+    [disabled, icon, isMobile, primary, secondary, ternary],
+  );
+}
+
 type ExperienceItem = {
   titleTranslationKey: string;
   company: string;
+  disabled?: boolean;
 };
-
 type ExperienceItemProps = ExperienceItem & AboutTimespan;
 
 function ExperienceListItem({
   titleTranslationKey,
   company,
+  disabled,
   from,
   to,
 }: ExperienceItemProps): JSX.Element {
   const { t } = useTranslation("experience");
-  const isMobile = useIsMobile();
   return (
-    <ListItem>
-      <ListItemIcon sx={{ color: (theme) => theme.palette.common.black }}>
-        <BusinessIcon fontSize="large" />
-      </ListItemIcon>
-      <ListItemText
-        disableTypography
-        primaryTypographyProps={{
-          color: (theme) => theme.palette.common.black,
-          fontWeight: (theme) => theme.typography.fontWeightBold,
-          fontSize: (theme) => theme.typography.pxToRem(isMobile ? 14 : 16),
-        }}
-        secondaryTypographyProps={{
-          color: (theme) => theme.palette.common.black,
-        }}
-        primary={
-          <Typography
-            sx={{
-              color: (theme) => theme.palette.common.black,
-              fontWeight: (theme) => theme.typography.fontWeightBold,
-              fontSize: (theme) => theme.typography.pxToRem(isMobile ? 14 : 16),
-            }}
-          >
-            {t(titleTranslationKey)}
-          </Typography>
-        }
-        secondary={
-          <>
-            <Typography
-              component={isMobile ? "p" : "span"}
-              sx={{
-                mr: 2,
-                color: (theme) => theme.palette.grey[600],
-              }}
-            >
-              {company}
-            </Typography>
-            {timespansToComponent(from, to)}
-          </>
-        }
-      />
-    </ListItem>
+    <AboutListItem
+      primary={t(titleTranslationKey)}
+      secondary={company}
+      disabled={disabled}
+      ternary={timespansToComponent(from, to)}
+      icon={<BusinessIcon fontSize="large" />}
+    />
+  );
+}
+
+type EducationItem = {
+  titleTranslationKey: string;
+  school: string;
+};
+type EducationItemProps = EducationItem & AboutTimespan;
+
+function EducationListItem({
+  titleTranslationKey,
+  school,
+  from,
+  to,
+}: EducationItemProps): JSX.Element {
+  const { t } = useTranslation("education");
+  return (
+    <AboutListItem
+      primary={t(titleTranslationKey)}
+      secondary={school}
+      ternary={timespansToComponent(from, to)}
+      icon={<SchoolIcon fontSize="large" />}
+    />
   );
 }
 
